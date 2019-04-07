bb2fa8b09dc558d918cd260767746cf34b9d563b
Roman <keks.tlt@gmail.com>
2019-04-07 21:59:39 +0300

    interface

--- src/IFileClass.php
+++ src/IFileClass.php
@@ -1,1 +1,14 @@
 <?php
+
+
+namespace MyFileLoader;
+
+interface IFileClass
+{
+	public function __construct($files_directory);
+	public function saveImage($imageName, $size = 7);
+	public function saveDocument($documentName, $size = 17);
+	public function deleteFile($fileName);
+	public function getRealName($filename);
+	public function setFileSizeLimit($limit = 5);
+}
